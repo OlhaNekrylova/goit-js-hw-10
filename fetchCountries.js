7021bc13e18b03fd5d154aa1f9b2adcc083645e5
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-12-07 18:49:49 +0200

    export

---
+++ fetchCountries.js
@@ -0,0 +1,6 @@
+export const fetchCountries = (name) => {
+    return fetch ()
+    .then(response => response.json(),
+    ); 
+}
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './css/styles.css';
+import 
 
 const DEBOUNCE_DELAY = 300; 
